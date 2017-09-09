8e87fcacb95d5e51e2846783cba5b6470ddede71
Cody Jenkins <cjenkins@dius.com.au>
2017-09-09 15:14:27 +1000

    Create pulser instrument

---
+++ src/instruments/Pulser.js
@@ -0,0 +1,41 @@
+import { noteToFreq } from '../keyboard';
+
+function createOsc(context, freq) {
+  const osc = context.createOscillator();
+  osc.frequency.value = freq;
+  osc.type = 'sine';
+  return osc;
+}
+
+const TIME_CONSTANT = 0.01;
+const DECAY_CONSTANT = 0.1;
+const DURATION = 0.1;
+
+export default class Pulse {
+  constructor(context) {
+    this.context = context;
+    this.oscillators = [
+      createOsc(this.context, noteToFreq('D2')),
+      createOsc(this.context, noteToFreq('A3')),
+      createOsc(this.context, noteToFreq('F#3')),
+    ];
+
+    this.maxGain = 1 / this.oscillators.length;
+    this.outputGain = this.context.createGain();
+    this.outputGain.gain.value = 0;
+    this.oscillators.forEach(o => {
+      o.start();
+      o.connect(this.outputGain);
+    });
+  }
+
+  play(velocity = 1) {
+    console.log(Math.min(1, velocity))
+    this.outputGain.gain.setTargetAtTime(Math.min(1, velocity) * this.maxGain, this.context.currentTime, TIME_CONSTANT);
+    this.outputGain.gain.setTargetAtTime(0, this.context.currentTime + DURATION, DECAY_CONSTANT);
+  }
+
+  connect(destination) {
+    this.outputGain.connect(destination);
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,89 +1,11 @@
-import { noteToFreq } from './keyboard';
+import Pulser from './instruments/Pulser'
 
 const AudioContext = window.AudioContext || window.webkitAudioContext;
 const context = new AudioContext();
 
-function createOsc(freq) {
-  const osc = context.createOscillator();
-  osc.frequency.value = freq;
-  osc.type = 'sine';
+const pulser = new Pulser(context);
+pulser.connect(context.destination);
 
-  return osc;
-}
+var i = 0;
 
-
-// Oscillators
-const all = [
-  createOsc(noteToFreq('D2')),
-  createOsc(noteToFreq('A3')),
-  // createOsc(noteToFreq('C#3')),
-  createOsc(noteToFreq('F#3')),
-  // createOsc(noteToFreq('D3')),
-];
-
-// Gains
-const pulseGainNode = context.createGain();
-pulseGainNode.gain.value = 0;
-
-const ambientGainNode = context.createGain();
-ambientGainNode.gain.value = 0;
-
-const volumeGainNode = context.createGain();
-volumeGainNode.gain.value = 2;
-
-// Convolver
-const convolverNode = context.createConvolver();
-const frameCount = context.sampleRate * 2;
-
-const convolverBuffer = context.createBuffer(2, frameCount, context.sampleRate);
-const channelData = [convolverBuffer.getChannelData(0), convolverBuffer.getChannelData(1)];
-
-const TARGET = .01;
-const LAMBDA = -Math.log(TARGET) / frameCount;
-for (var i = 0; i < frameCount; i++) {
-  var envelope = Math.exp(-LAMBDA * i);
-  channelData[0][i] = (Math.random() * 2 - 1) * envelope;
-  channelData[1][i] = (Math.random() * 2 - 1) * envelope;
-}
-convolverNode.buffer = convolverBuffer;
-convolverNode.normalize = false;
-
-// Compressor
-const compressorNode = context.createDynamicsCompressor();
-compressorNode.threshold.value = -24;
-compressorNode.knee.value = 30;
-compressorNode.ratio.value = 12;
-compressorNode.attack.value = 0;
-compressorNode.release.value = 0.25;
-
-// Network
-all.forEach(oscN => {
-  oscN.connect(pulseGainNode);
-  oscN.connect(ambientGainNode);
-});
-ambientGainNode.connect(convolverNode);
-pulseGainNode.connect(convolverNode);
-pulseGainNode.connect(volumeGainNode);
-convolverNode.connect(volumeGainNode);
-
-// volumeGainNode.connect(context.destination);
-
-volumeGainNode.connect(compressorNode);
-compressorNode.connect(context.destination);
-
-// Start
-all.forEach(oscN => oscN.start());
-
-// const
-
-const TIME_CONSTANT = 0.01;
-const DECAY_CONSTANT = 0.1;
-const DURATION = 0.1;
-
-function playBeat() {
-  pulseGainNode.gain.setTargetAtTime(.2, context.currentTime, TIME_CONSTANT);
-  pulseGainNode.gain.setTargetAtTime(0, context.currentTime + DURATION, DECAY_CONSTANT);
-}
-
-// playBeat();
-// setInterval(playBeat, 1500);
+window.addEventListener('keydown', () => pulser.play());
